aaa4a0f62d0ec21dc25d8eb5c88d0a3ed933c9aa
Glitch (numberstomusic) <none>
2019-10-19 21:04:00 +0000

    🎑🎆 Checkpoint ./script.js:2055672/1963

--- script.js
+++ script.js
@@ -11,8 +11,13 @@ var F = document.getElementById("pianoF");
 var G = document.getElementById("pianoG");
 
 
+var note1;
+var note2;
+
 function getNumber () {
   var mynumber = document.getElementById("mainInput").value;
+  var length = mynumber.toString().length;
+  length = length - 1;
   var number = mynumber,
     output = [],
     sNumber = number.toString();
@@ -20,10 +25,32 @@ function getNumber () {
   for (var i = 0, len = sNumber.length; i < len; i += 1) {
     output.push(+sNumber.charAt(i));
   }  
-  if (output == 1) {
+  
+  note1 = output[length - length];
+  var note2 = output[length - length + 1];
+  var note3 = output[length - length + 2];
+  var note4 = output[length - length + 3];
+  var note5 = output[length - length + 4];
+  var note6 = output[length - length + 5];
+  var note7 = output[length - length + 6];
+  var note8 = output[length - length + 7];
+  var note9 = output[length - length + 8];
+  var note10 = output[length - length + 9];
+  
+  playNotes();
+  
+}
+
+function playNotes() {
+  if (note1 == 0) {
     A.play();
+  } else {
+    if (note1 == 1) {
+      B.play();
+    }
   }
 }
+
 
 $(document).on('keypress',function(e) {
     if(e.which == 13) {
