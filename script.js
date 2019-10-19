c206672071dae59ffe911ab99bb7cc2a53c560fe
Glitch (numberstomusic) <none>
2019-10-19 20:28:00 +0000

    🎡👻 Checkpoint ./index.html:2055672/707 ./script.js:2055672/775

--- script.js
+++ script.js
@@ -2,6 +2,21 @@ var input = document.getElementById('mainInput');
 input.focus();
 input.select();
 
-var audio = new Audio('Piano A.wav');
-audio.play();
+var A = document.getElementById("pianoA");
+var B = document.getElementById("pianoB");
+var C = document.getElementById("pianoC");
+var D = document.getElementById("pianoD");
+var E = document.getElementById("pianoE");
+var F = document.getElementById("pianoF");
+var G = document.getElementById("pianoG");
 
+function getNumber () {
+  var number = document.getElementById("mainInput").value;
+  alert(number);
+}
+
+$(document).on('keypress',function(e) {
+    if(e.which == 13) {
+        getNumber();
+    }
+});
