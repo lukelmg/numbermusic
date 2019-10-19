c6900bed163251410a5912bc0878577ca9d96df5
Glitch (numberstomusic) <none>
2019-10-19 21:52:00 +0000

    📌👚 Checkpoint ./script.js:2055672/1461

--- script.js
+++ script.js
@@ -4,6 +4,15 @@ input.select();
 
 var timeBetween = 300;
 
+var A = document.getElementById("pianoA");
+  var B = document.getElementById("pianoB");
+  var C = document.getElementById("pianoC");
+  var D = document.getElementById("pianoD");
+  var E = document.getElementById("pianoE");
+  var F = document.getElementById("pianoF");
+  var G = document.getElementById("pianoG");
+
+
 var note1;
 var note2;
 var note3;
@@ -15,7 +24,13 @@ var note8;
 var note9;
 var note10;
 
-var noteA
+var noteA = [1,8];
+var noteB = [2,9];
+var noteC = [3,0];
+var noteD = 4;
+var noteE = 5;
+var noteF = 6;
+var noteG = 7;
 
 function getNumber () {
   var mynumber = document.getElementById("mainInput").value;
@@ -47,23 +62,21 @@ function getNumber () {
 }
 
 function playNotes() {
-  
-  var A = document.getElementById("pianoA");
-  var B = document.getElementById("pianoB");
-  var C = document.getElementById("pianoC");
-  var D = document.getElementById("pianoD");
-  var E = document.getElementById("pianoE");
-  var F = document.getElementById("pianoF");
-  var G = document.getElementById("pianoG");
-  
-  if (note1 == 0)
-  
-
- 
-
- 
+  detectNote1();
 }
 
+
+function detectNote1() {
+  
+  
+  if (note1 == 1 || note1 == 8) {A.play();}
+  if (note1 == 2 || note1 == 9) {B.play();}
+  if (note1 == 3 || note1 == 0) {C.play();}
+  if (note1 == 4) {D.play();}
+  if (note1 == 5) {E.play();}
+  if (note1 == 6) {F.play();}
+  if (note1 == 7) {G.play();}
+}
 
 
 $(document).on('keypress',function(e) {
