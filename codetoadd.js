318b97c892dc7fe6f834bd55ff6c3509638ad3a3
Glitch (numbermusic) <none>
2019-10-25 14:02:37 +0000

    arrays are the key

--- codetoadd.js
+++ codetoadd.js
@@ -1,12 +1,14 @@
 function letterValue(str){
-    var anum={
+    var cmajorNotes={
         a: 1, b: 2, c: 3, d: 4, e: 5, f: 6, g: 7, h: 8, i: 9, j: 10, k: 11, 
         l: 12, m: 13, n: 14,o: 15, p: 16, q: 17, r: 18, s: 19, t: 20, 
         u: 21, v: 22, w: 23, x: 24, y: 25, z: 26
     }
-    if(str.length== 1) return anum[str] || ' ';
-    return str.split('').map(letterValue);
 }
+
+
+
+var cmajNotes = ['C5', 'Csharp5', 'D5', 'Eflat5', 'E5', 'F5', ]
 
 
 //Should allow me to set each note to a different variable depending on the scale that is being played
