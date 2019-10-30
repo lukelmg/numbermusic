550318c80a7f654c9d8695b6ec2cc815b617f3ff
Glitch (numbermusic) <none>
2019-10-30 01:36:39 +0000

    working select scale with 2 scales

--- codetoadd.js
+++ codetoadd.js
@@ -1,9 +1,8 @@
 function letterValue(str){
     var cmajorNotes={
-        a: 1, b: 2, c: 3, d: 4, e: 5, f: 6, g: 7, h: 8, i: 9, j: 10, k: 11, 
-        l: 12, m: 13, n: 14,o: 15, p: 16, q: 17, r: 18, s: 19, t: 20, 
-        u: 21, v: 22, w: 23, x: 24, y: 25, z: 26
+        C5: 1, D5: 2, E5: 3, F5: 4, G5: 5, A5: 6, B5: 7, C6: 8, D6: 9, E6: 0
     }
+    alert(cmajorNotes);
 }
 
 
--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var input = document.getElementById("mainInput");
 input.focus();
 input.select();
 
-var timeBetween = 250;
+var timeBetween = 10;
 var noteDuration = 250;
 
 var A = document.getElementById("pianoA");
@@ -19,8 +19,10 @@ var lengthcount = 0;
 
 var letterNote = "A";
 
+var mynumber;
+
 function getNumber() {
-  var mynumber = document.getElementById("mainInput").value; //get number of input
+   mynumber = document.getElementById("mainInput").value; //get number of input
   var length = mynumber.toString().length; //calculate length of input
 
   var number = mynumber,
@@ -31,7 +33,7 @@ function getNumber() {
     setTimeout(function() {
       output.push(+sNumber.charAt(lengthcount)); //change i to lengthcount
       note1 = output[length - length + lengthcount];
-      detectNote1();
+      convertSelectToFunctions();
 
       noteToPlay[letterNote]();
 
@@ -55,80 +57,114 @@ var frequency = 440.0;
 var context = new AudioContext();
 var o = context.createOscillator();
 
+var C5 = 523;
+var Csharp5 = 554;
+var D5 = 587;
+var Eflat5 = 622;
+var E5 = 659;
+var F5 = 698;
+var Fsharp5= 739;
+var G5 = 783.99;
+var Gsharp5 = 830.61;
+var A5 = 880;
+var Bflat5 = 932;
+var B5 = 987;
+
+var C6 = C5 * 2;
+var Csharp6 = Csharp5 * 2;
+var D6 = D5 * 2;
+var Eflat6 = Eflat5;
+var E6 = E5 * 2;
+var F6 = F5 * 2;
+var Fsharp6 = Fsharp5 * 2;
+var G6 = G5 * 2;
+var Gsharp6 = Gsharp5 * 2;
+var A6 = A5 * 2;
+var Bflat6 = Bflat5 * 2;
+var B6 = B5 * 2;
+
+var nanValue = NaN;
+
+
 var noteToPlay = {
+  
+  pause: function() {
+    playNote(1, noteDuration);
+  },
+  
   C5: function() {
-    playNote(523.25, noteDuration);
+    playNote(C5, noteDuration);
   },
   Csharp5: function() {
-    playNote(554.37, noteDuration);
+    playNote(Csharp5, noteDuration);
   },
   D5: function() {
-    playNote(587.33, noteDuration);
+    playNote(D5, noteDuration);
   },
   Eflat5: function  () {
-    playNote(622.25, noteDuration);
+    playNote(Eflat5, noteDuration);
   },
   E5: function () {
-    playNote(659.25, noteDuration);
+    playNote(E5, noteDuration);
   },
   F5: function  () {
-    playNote(698.46, noteDuration);
+    playNote(F5, noteDuration);
   },
   Fsharp5: function () {
-    playNote(739.99, noteDuration);
+    playNote(Fsharp5, noteDuration);
   },
   G5: function  () {
-    playNote(783.99, noteDuration);
+    playNote(G5, noteDuration);
   },
   Gsharp5: function () {
-    playNote(830.61, noteDuration);
+    playNote(Gsharp5, noteDuration);
   },
   A5: function () {
-    playNote(880.00, noteDuration);
+    playNote(A5, noteDuration);
   },
   Bflat5: function() {
-    playNote(932.33, noteDuration);
+    playNote(Bflat5, noteDuration);
   },
   B5: function () {
-    playNote(987.77, noteDuration);
+    playNote(B5, noteDuration);
   },
   
   
   C6: function() {
-    playNote(1046.50, noteDuration);
+    playNote(C6, noteDuration);
   },
   Csharp6: function() {
-    playNote(1108.73, noteDuration);
+    playNote(Csharp6, noteDuration);
   },
   D6: function() {
-    playNote(1174.66, noteDuration);
+    playNote(D6, noteDuration);
   },
   Eflat6: function  () {
-    playNote(1244.51, noteDuration);
+    playNote(Eflat6, noteDuration);
   },
   E6: function () {
-    playNote(1318.51, noteDuration);
+    playNote(E6, noteDuration);
   },
   F6: function  () {
-    playNote(1396.91, noteDuration);
+    playNote(F6, noteDuration);
   },
   Fsharp6: function () {
-    playNote(1479.98, noteDuration);
+    playNote(Fsharp6, noteDuration);
   },
   G6: function  () {
-    playNote(1567.98, noteDuration);
+    playNote(G6, noteDuration);
   },
   Gsharp6: function () {
-    playNote(1661.22, noteDuration);
+    playNote(Gsharp6, noteDuration);
   },
   A6: function () {
-    playNote(1760.00, noteDuration);
+    playNote(A6, noteDuration);
   },
   Bflat6: function() {
-    playNote(1864.66, noteDuration);
+    playNote(Bflat6, noteDuration);
   },
   B6: function () {
-    playNote(1975.53, noteDuration);
+    playNote(B6, noteDuration);
   }
  
 };
@@ -163,6 +199,12 @@ function detectNote1() {
                   } else {
                     if (note1 == 0) {
                       letterNote = "E6"
+                    } else {
+                     
+                    
+                        letterNote = "pause";
+                        
+                      
                     }
                   }
                 }
@@ -207,6 +249,8 @@ function cmajor() {
                   } else {
                     if (note1 == 0) {
                       letterNote = "E6"
+                    }  else {
+                      letterNote = "pause";
                     }
                   }
                 }
@@ -218,6 +262,52 @@ function cmajor() {
     }
   }
 }
+
+function dmajor() {
+  if (note1 == 1) {
+    letterNote = "D5";
+  } else {
+    if (note1 == 2) {
+      letterNote = "E5";
+    } else {
+      if (note1 == 3) {
+        letterNote = "Fsharp5";
+      } else {
+        if (note1 == 4) {
+          letterNote = "G5";
+        } else {
+          if (note1 == 5) {
+            letterNote = "A5";
+          } else {
+            if (note1 == 6) {
+              letterNote = "B5";
+            } else {
+              if (note1 == 7) {
+                letterNote = "Csharp6";
+              } else {
+                if (note1 == 8) {
+                  letterNote = "D6";
+                } else {
+                  if (note1 == 9) {
+                    letterNote = "E6"; 
+                  } else {
+                    if (note1 == 0) {
+                      letterNote = "Fsharp6"
+                    } else {
+                      letterNote = "pause";
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+
 
 $(document).on("keypress", function(e) {
   if (e.which == 13) {
@@ -248,4 +338,37 @@ function playNote(frequency, duration) {
   setTimeout(function() {
     oscillator.stop();
   }, duration);
+}
+
+letterValue();
+function letterValue(str){
+    //var cmajorNotes={
+    //    C5: 1, D5: 2, E5: 3, F5: 4, G5: 5, A5: 6, B5: 7, C6: 8, D6: 9, E6: 0,
+    //}
+    
+    var cmajNotes = ['C5', 'Csharp5', 'D5', 'Eflat5', 'E5', 'F5', ]
+    
+    //alert(cmajNotes);
+}
+
+
+
+var selectedScale  = "cmajor";
+
+
+var readFromSelect;
+
+function readSelect() {
+   readFromSelect = document.getElementById("scales").value;
+}
+
+
+function convertSelectToFunctions() {
+  if (readFromSelect == "C Major") {
+    cmajor();
+  } else {
+    if (readFromSelect == "D Major") {
+      dmajor();
+    }
+  }
 }
